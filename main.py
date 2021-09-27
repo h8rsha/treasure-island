ffd5f3a90e253c0f09425c4899b24884f2ee1353
Harsha Vardhan B <80052719+h8rsha@users.noreply.github.com>
2021-09-27 17:05:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-direction = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'")
+direction = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n")
 
 if(direction == "left"):
-  swim_or_wait = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.").lower()
+  swim_or_wait = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n").lower()
   if(swim_or_wait == "wait"):
-    door_chosen = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
+    door_chosen = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower()
     if(door_chosen == "red"):
       print("Burned by fire. Game Over")
     elif (door_chosen == "yellow"):
